5889be05ea64061d03e36ce03273e57a9a5fd6db
Hamed Rostami <hamedrostami@tuta.io>
2020-04-28 17:44:57 +0430

    1.create global placeholder 2.create function foroperation of case

--- 2fa.bash
+++ 2fa.bash
@@ -1,24 +1,105 @@
-#!/bin/bash
+#! bin/bash
 
-RED='\033[0;31m'
-pass 2fa/
-echo -e "Enter a provider name:\n"
-read -t 15 key_name
+#============================
+AUTHOR="Hamed Rostami"
+EMAIL="hamedrostami@tuta.io"
+VERION="1.0.0"
+#============================
 
-if [ -z "$key_name" ]
-then
-    printf "${RED} I have been waiting for you for a long time, it is enough to wait, Bye.\n\n"
-    exit 1
-else
 
-    if key=$(pass 2fa/$key_name)
-    then
-        if result=$(oathtool --base32 --totp "$key") 
-        then
-            echo $result|xclip -selection clipboard
-            echo "Copied $key_name key to clipboard, it will clear after 20 second."
-        fi
-    fi
-fi
+RED='\033[0;31m' #color used in printf output
+
+
+2fa_crud(){
+    pass ${operation} 2fa/"$2"
+}
+
+get_otp_key(){
+     if provider=$(pass 2fa/"$1")
+                then
+                    if result=$(oathtool --base32 --totp "$provider") 
+                    then
+                        echo $result|xclip -selection clipboard
+                        printf "Copied $1 key to clipboard,$RED it will clear after 20 second.\n"
+                    fi
+                fi
 sleep 20 && xclip -selection clipboard /dev/null&
 
+
+}
+
+
+get_help(){
+    cat <<ASK
+    pass: the standard unix password manager OTP extention =
+    =                                                      =
+    =                       v$VERION                         =
+    =                                                      =
+    =                 Author : $AUTHOR               = 
+    =                  $EMAIL                =
+    =                                                      =
+    =                                                      =
+    ========================================================
+
+    Usage:
+        pass 
+            List providers.
+
+        pass insert provider-name
+            Insert new key. Optionally, echo the password back to the console
+            during entry. Or, optionally, the entry may be multiline. Prompt before
+            overwriting existing password unless forced.
+
+        pass edit provider
+            Insert a new  key or edit an existing key using editor.
+
+        pass rm provider 
+            Remove existing password or directory, optionally forcefully.
+
+        pass help
+            Show this text.
+
+        pass version
+            Show version information.
+ASK
+
+
+}
+
+
+case "$1" in 
+        "") pass 2fa/
+            exit ;;
+    insert|ins*) if [ -z "$2" ]
+                 then
+                     echo "Usage: 2fa insert key-name"
+                 else
+                     operation="insert"
+                     2fa_crud "$operation" "$2"
+                fi ;;
+      edit|edi*) if [ -z "$2" ]
+                 then
+                     echo "Usage: 2fa edit key-name"
+                 else
+                     operation="edit"
+                     2fa_crud "$operation" "$2"
+                fi ;;
+
+    remove|rem*|rm) if [ -z "$2" ]
+                 then
+                     echo "Usage: 2fa remove key-name"
+                 else
+                     operation="remove"
+                     2fa_crud "$operation" "$2"
+                fi ;;
+
+        -v) echo "$VERION" 
+             ;;
+
+         -h) get_help
+              ;; 
+  
+    *) get_otp_key "$1"
+        ;;
+
+esac
