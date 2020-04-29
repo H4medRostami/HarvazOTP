67f3bc37c90860c28594228e52150230e839f667
Hamed Rostami <hamedrostami@tuta.io>
2020-04-29 22:14:58 +0430

    add color to prompt

--- 2fa.bash
+++ 2fa.bash
@@ -6,10 +6,6 @@ EMAIL="hamedrostami@tuta.io"
 VERION="1.0.0"
 #============================
 
-
-RED='\033[0;31m' #color used in printf output
-
-
 2fa_crud(){
     pass ${operation} 2fa/"$2"
     return
@@ -20,9 +16,10 @@ get_otp_key(){
                 then
                     if result=$(oathtool --base32 --totp "$provider") 
                     then
-                        echo $result|xclip -selection clipboard
-                        printf "Copied $1 key to clipboard,$RED it will clear after 20 second.\n"
-                    fi
+                        echo $result|xclip -selection clipboard 
+                        printf "\e[7m\e[32mCopied  key to clipboard\e[0m,\e[91m\e[7mit will clear after \e[5m20 \e[25msecond.\e[0m\n"
+
+                     fi
                 fi
 sleep 20 && xclip -selection clipboard /dev/null&
 return 
@@ -52,7 +49,7 @@ get_help(){
         pass edit provider
             Insert a new  key or edit an existing key using editor.
 
-        pass rm provider 
+        pass rm provider
             Remove existing password or directory, optionally forcefully.
 
         pass help
@@ -71,7 +68,7 @@ case "$1" in
             exit ;;
     insert|ins*) if [ -z "$2" ]
                  then
-                     echo "Usage: 2fa insert key-name"
+                     echo -e "\e[1mUsage: \e[0m\e[7m\e[96m2fa insert key-name\e[0m"
                  else
                      operation="insert"
                      2fa_crud "$operation" "$2"
@@ -79,7 +76,7 @@ case "$1" in
                 exit ;;
       edit|edi*) if [ -z "$2" ]
                  then
-                     echo "Usage: 2fa edit key-name"
+                     echo -e "\e[1mUsage: \e[0m\e[7m\e[96m2fa edit key-name\e[0m"
                  else
                      operation="edit"
                      2fa_crud "$operation" "$2"
@@ -88,7 +85,7 @@ case "$1" in
 
     remove|rem*|rm) if [ -z "$2" ]
                  then
-                     echo "Usage: 2fa remove key-name"
+                     echo -e "\e[1mUsage: \e[0m\e[7m\e[96m2fa remove key-name\e[0m"
                  else
                      operation="remove"
                      2fa_crud "$operation" "$2"
